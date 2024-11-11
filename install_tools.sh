16cd95e671ef1b289a4cbb538c76be97346f9c48
Adam Sasmita <firdamdamsasmita@upi.edu>
2024-11-11 15:01:03 +0700

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Menentukan direktori tempat alat akan diinstal
 INSTALL_DIR="$HOME/tools"
 mkdir -p "$INSTALL_DIR"
 cd "$INSTALL_DIR"
 
-# Fungsi untuk mengkloning repositori dari GitHub
 git_clone() {
     local repo_url=$1
     local tool_name=$2
@@ -18,28 +16,15 @@ git_clone() {
     fi
 }
 
-# Menginstal alat dari GitHub
 echo "Memulai pengunduhan alat..."
 
-# CMSeeK
 git_clone "https://github.com/Tuhinshubhra/CMSeeK.git" "CMSeeK"
-
-# SubDomainizer
 git_clone "https://github.com/nsonaniya2010/SubDomainizer.git" "SubDomainizer"
-
-# Sublist3r
 git_clone "https://github.com/aboul3la/Sublist3r.git" "Sublist3r"
-
-# WhatWeb
 git_clone "https://github.com/urbanadventurer/WhatWeb.git" "WhatWeb"
-
-# ParamSpider
 git_clone "https://github.com/devanshbatham/ParamSpider.git" "ParamSpider"
-
-# jshole
 git_clone "https://github.com/callforpapers-source/jshole.git" "jshole"
 
-# Menginstal whois menggunakan apt-get jika belum terinstal
 echo "Memeriksa dan menginstal whois..."
 if ! command -v whois &> /dev/null; then
     echo "whois tidak ditemukan, menginstal whois..."
@@ -54,7 +39,5 @@ else
 fi
 
 echo "Semua alat telah diunduh dan diinstal!"
-
-# Menyelesaikan skrip
 echo "Skrip selesai! Anda dapat memulai penggunaan alat-alat tersebut."
 
